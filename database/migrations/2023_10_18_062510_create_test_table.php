4f6bc92e0cb5f006b277a4c28d0e0d85d97c9112
LUN-ACER-SFX14\kurod <kurodaniji@gmail.com>
2023-10-18 14:40:24 +0800

    123

--- database/migrations/2023_10_18_062510_create_test_table.php
+++ database/migrations/2023_10_18_062510_create_test_table.php
@@ -16,6 +16,7 @@ class CreateTestTable extends Migration
         Schema::create('tests', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            
         });
     }
 
