9ae433670b8a047b8dd10e08bb08e588413a723c
BuildTools <unconfigured@null.spigotmc.org>
2023-11-29 12:27:22 +0800

    e1094181205 add 1.表格呈現 brand.index (view); 2. 增加show、edit

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -1,25 +1,38 @@
-<html>
-    <body>
+@extends('app')
 
+@section('title',汽車資料庫管理-列出所有品牌')
 
+@section('car_contents')
         <h1>
-            品牌
+            所有品牌
         </h1>
-
-
+        <table>
+            <tr>
+                <th>編號</th>
+                <th>品牌</th>
+                <th>總部</th>
+                <th>行政區域</th>
+                <th>成立時間</th>
+                <th>官方網址</th>
+                <th>操作1</th>
+                <th>操作2</th>
+                <th>操作3</th>
+            </tr>
         @for($i=0;$i<count(brands);$i++)
-        {{$brands[$i]['brands']}}</br>
-        {{$brands[$i]['headquarter']}}</br>
-        {{$brands[$i]['area']}}</br>
-        {{$brands[$i]['year']}}</br>
-        {{$brands[$i]['html']}}</br></br>
+       
+            <tr>
+            <td>{{$brands[$i]['id']}}</td>
+            <td>{{$brands[$i]['brands']}}</td>
+            <td>{{$brands[$i]['headquarter']}}</td>
+            <td>{{$brands[$i]['area']}}</td>
+            <td>{{$brands[$i]['year']}}</td>
+            <td>{{$brands[$i]['html']}}</td>
+            <td><a href="{{route('brands.show',['id'=>$brands[$i]['id']]}}">顯示</a></td>
+            <td><a href="{{route('brands.edit',['id'=>$brands[$i]['id']]}}">編輯</a></td>
+            <td>刪除</td>
+            </tr>
+        
         @endfor
-
-    </body>
+        </table>
     
-    <head>
-<title>
-    品牌
-</title>
-    </head>
-</html>
+@endsection
