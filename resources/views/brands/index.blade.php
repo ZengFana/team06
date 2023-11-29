cd65ae6e6dc13e260ce918de5b1b520eff1be7c4
E1094181206 <E1094181206>
2023-11-29 11:48:31 +0800

    增加版模配置 app, footer, header, 並修改 brands.index, 及 types.index 等視圖

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -1,15 +1,28 @@
-<html>
-    <head>
-        <title>列出所有品牌</title>
-    </head>
-    <body>
+</head>
+        <body>
         <h1>列出所有品牌</h1>
+        <table>
+            <tr>
+                <th>編號</th>
+                <th>品牌</th>
+                <th>總部</th>
+                <th>行政區域</th>
+                <th>成立時間</th>
+                <th>官方網址</th>
+            </tr>
         @for($i=0;$i<count($brands);$i++)
-        {{$brands[$i]['brands']}}</br>
-        {{$brands[$i]['headquarter']}}</br>
-        {{$brands[$i]['area']}}</br>
-        {{$brands[$i]['year']}}</br>
-        {{$brands[$i]['html']}}</br></br>
-        @endfor
+        <tr>
+        <td>{{$brands[$i]['id']}}</td>
+        <td>{{$brands[$i]['brands']}}</td>
+        <td>{{$brands[$i]['headquarter']}}</td>
+        <td>{{$brands[$i]['area']}}</td>
+        <td>{{$brands[$i]['year']}}</td>
+        <td>{{$brands[$i]['html']}}</td>
+        <ta><a href="{{route(brands.show',['id'=>$brands[$i]['id']}}">顯示</a></td>
+        <ta><a href="{{route(brands.edit',['id'=>$brands[$i]['id']}}">修改</a></td>
+        <td>刪除</td>
+    </tr>
+    @endfor
+</table>
     </body>
 </html>
